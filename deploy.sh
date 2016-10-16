e7aef4e5240f30897d582af817ca1a6e8d4d32a3
Dolan Miu <dolan_miu@hotmail.com>
2016-10-16 01:19:04 +0100

    deploy options

--- deploy.sh
+++ deploy.sh
@@ -11,4 +11,4 @@ cd dist
 git init
 git add .
 git commit -m "Deploy to Github Pages"
-git push --force --quiet "https://${GH_TOKEN}@$github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@github.com/${GITHUB_REPO}.git" master:gh-pages > /dev/null 2>&1
