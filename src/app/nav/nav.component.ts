7edc76bdf95e4ebbf2cf6d92211404e900585417
Dolan Miu <dolan_miu@hotmail.com>
2016-10-19 23:01:13 +0100

    added initial content

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-
+class NavTab {
+    text: string;
+    route: string;
+}
 @Component({
     selector: 'app-nav',
     templateUrl: './nav.component.html',
     styleUrls: ['./nav.component.scss']
 })
 export class NavComponent {
+
+    links: Array<NavTab> = [
+        {
+            text: 'About Us',
+            route: '#about'
+        }
+    ];
 
 }
